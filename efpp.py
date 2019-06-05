94a895813a1af1b71fa27fea0b889f541364405d
Fujita7 <38450018+Fujita7@users.noreply.github.com>
2019-06-05 11:18:41 +0900

    Revise debug (#2)
    * Revise debugp by Fujita7

--- efpp.py
+++ efpp.py
@@ -729,6 +729,8 @@ def debugp_decode(lines_in):
                 line += ';'
             line += 'print *, '
             line += '\"\\\\__MODULE__(__LINE__): \", '
+            countParenthesis = 0
+            temp = ''
             for arg in args:
                 a = arg.strip() # put off blanks
                 if a[0]=='\'' or a[0]=='\"':  # string (e.g., 'message')
@@ -738,6 +740,21 @@ def debugp_decode(lines_in):
                         line += a + ', ' + a[0] + ', '
                 elif a[-1]=='\'' or a[-1]=='\"':# string (e.g., '..., smthng')
                     line +=  a[-1] + a + ', '
+                elif '(' in a or countParenthesis!=0:
+                    if '(' in a:
+                        countParenthesis += a.count('(') - a.count(')')
+                        temp += a + ', '
+                        if countParenthesis == 0:
+                            temp = temp[:-2]  # put of the last "comma + space"
+                            line += "\" " + temp + " = \", " + temp + ', '
+                    elif ')' in a:
+                        countParenthesis += a.count('(') - a.count(')')
+                        temp += a + ', '
+                        if countParenthesis == 0:
+                            temp = temp[:-2]  # put of the last "comma + space"
+                            line += "\" " + temp + " = \", " + temp + ', '
+                    else:
+                        temp += a + ', '
                 else:
                     line += "\" " + a + " = \", " + a + ', '
             line = line[:-2]  # put of the last "comma + space"
